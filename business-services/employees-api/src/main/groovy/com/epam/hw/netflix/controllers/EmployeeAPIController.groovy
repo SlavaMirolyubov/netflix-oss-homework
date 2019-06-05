4e32b395d97d84d6638be1f7038e4479db2f78b7
Slava <grandwhiskey@yandex.ru>
2019-06-05 18:32:17 +0400

    Hystix added

--- business-services/employees-api/src/main/groovy/com/epam/hw/netflix/controllers/EmployeeAPIController.groovy
+++ business-services/employees-api/src/main/groovy/com/epam/hw/netflix/controllers/EmployeeAPIController.groovy
@@ -1,14 +1,21 @@
 package com.epam.hw.netflix.controllers
 
 import com.epam.hw.netflix.api.WorkspaceAPI
+import com.epam.hw.netflix.domain.Workspace
 import com.epam.hw.netflix.services.EmployeeService
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cloud.netflix.hystrix.EnableHystrix
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+import static com.epam.hw.netflix.domain.OSFamily.WINDOWS
+import static java.util.UUID.randomUUID
+
 @RestController
 @RequestMapping("/employees")
+@EnableHystrix
 class EmployeeAPIController {
 
     @Autowired
@@ -18,6 +25,7 @@ class EmployeeAPIController {
     WorkspaceAPI workspaceAPIClient
 
     @RequestMapping("/{id}")
+    @HystrixCommand(fallbackMethod = 'getDefault')
     def describeEmployee(@PathVariable("id") String id) {
         def employee = employeeService.findEmployee(id)
 
@@ -27,6 +35,17 @@ class EmployeeAPIController {
                 lastName : employee.lastName,
                 email    : employee.email,
                 workspace: workspaceAPIClient.getWorkspaceById(id) // null? Nope. Let's request exact workspace by employee.workspaceId from workspaces-api. How? With feign client maybe?
+        ]
+    }
+
+    def getDefault(String id) {
+        def employee = employeeService.findEmployee(id)
+        [
+                id       : employee.id,
+                firstName: employee.firstName,
+                lastName : employee.lastName,
+                email    : employee.email,
+                workspace: new Workspace("0000001", 1, 1, randomUUID().toString(), WINDOWS) // null? Nope. Let's request exact workspace by employee.workspaceId from workspaces-api. How? With feign client maybe?
         ]
     }
 }
